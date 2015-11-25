c68550cde5028cfa2cef0fa260bcaf61c85b267a
Carl König <made.swede@live.com>
2015-11-25 10:52:17 +0100

    fungerar

--- content/js/index.js
+++ content/js/index.js
@@ -1,4 +1,3 @@
-console.log("loaded");
 var module = angular.module("AnnonsApp", ["ui.router"]);
 
 module.config(function ($urlRouterProvider, $stateProvider) {
@@ -7,13 +6,8 @@ module.config(function ($urlRouterProvider, $stateProvider) {
         url: "/"
     });
 });
-
-module.controller("annonsCtrl", function ($scope, $http) {
-    var basurl = "http://api.arbetsformedlingen.se/platsannons/";
-    var query = "nyckelord=programmerare";
-    var sokMetod = "matchning?";
-    var lanMetod = "soklista/lan";
-
+var test;
+module.controller("annonsCtrl", function ($scope, getService) {
     /*$.ajax({
      type: 'GET',
      url: 'http://api.arbetsformedlingen.se/platsannons/matchning?nyckelord=programmerare',
@@ -33,12 +27,86 @@ module.controller("annonsCtrl", function ($scope, $http) {
      console.log("fail");
      }
      });*/
-    $http.get(basurl+sokMetod+query).success(function (data) {
-        $scope.annonser = data;
-        console.log(data);
-    });
-    $http.get(basurl+lanMetod).success(function (data) {
+
+    var promiseLan = getService.getLan();
+    promiseLan.then(function (data) {
         $scope.lan = data;
         console.log(data);
     });
+
+    var promiseSok = getService.getSearch("a", "", "");
+    promiseSok.then(function (data) {
+        $scope.annonser = data.matchningslista.matchningdata;
+        test = data;
+        console.log("vvvvvvvvvvvvv");
+        console.log($scope.annonser);
+        console.log("^^^^^^^^^^^^");
+    });
+
+    $scope.sok = function () {
+        var nyckelord = "" + $scope.nyckelord;
+        var lanid = "" + $scope.lanid;
+        var kommunid = "" + $scope.kommunid;
+        promiseSok = getService.getSearch(nyckelord, lanid, kommunid);
+    };
+    $scope.getKommuner = function () {
+        var lanid = "" + $scope.lanid;
+        promiseKommuner = getService.getKommuner(lanid);
+    };
+    $scope.datum = function(skitdatum){
+        var datum = skitdatum.split("T")[0];
+        return datum;
+    };
+});
+
+module.service("getService", function ($http, $q) {
+    this.getSearch = function (nyckelord, lanid, kommunid) {
+        var basurl = "http://api.arbetsformedlingen.se/platsannons/";
+        var sokMetod = "matchning";
+        var query = "?nyckelord=" + nyckelord;
+        query += "&lanid=" + lanid;
+        query += "&kommunid" + kommunid;
+        var deferred = $q.defer();
+        $http.get(basurl + sokMetod + query)
+                .success(function (data, status) {
+                    console.log(status);
+                    deferred.resolve(data);
+                }).error(function (data, status) {
+            console.log(status);
+            deferred.reject();
+        });
+        return deferred.promise;
+    };
+
+    this.getLan = function () {
+        var basurl = "http://api.arbetsformedlingen.se/platsannons/";
+        var lanMetod = "soklista/lan";
+        var deferred = $q.defer();
+        $http.get(basurl + lanMetod)
+                .success(function (data, status) {
+                    console.log(status);
+                    deferred.resolve(data);
+                }).error(function (data, status) {
+            console.log(status);
+            deferred.reject();
+        });
+        return deferred.promise;
+    };
+
+    this.getKommuner = function (lanid)
+    {
+        var basurl = "http://api.arbetsformedlingen.se/platsannons/";
+        var kommunMetod = "soklista/kommuner";
+        var query = "?lanid=" + lanid;
+        var deferred = $q.defer();
+        $http.get(basurl + kommunMetod + query)
+                .success(function (data, status) {
+                    console.log(status);
+                    deferred.resolve(data);
+                }).error(function (data, status) {
+            console.log(status);
+            deferred.reject();
+        });
+        return deferred.promise;
+    };
 });
